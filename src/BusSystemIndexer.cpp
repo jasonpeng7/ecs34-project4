96c6ea5d3668c3d1159f6b59456d70c75edb4518
jasonpeng7 <144397187+jasonpeng7@users.noreply.github.com>
2026-03-06 19:40:07 +0000

    added route by node id function and build map for route by node id

--- src/BusSystemIndexer.cpp
+++ src/BusSystemIndexer.cpp
@@ -13,10 +13,21 @@ struct CBusSystemIndexer::SImplementation{
 
     // routes
     std::vector<std::shared_ptr<SRoute>> DSortedRoutesByIndex;
-    std::unordered_map<TNodeID,std::shared_ptr<SRoute>> DRoutesByNodeID;
+
+    struct NodeIDPairHash {
+        std::size_t operator()(const std::pair<TNodeID, TNodeID> &pair) const {
+            std::size_t First = pair.first;
+            std::size_t Second = pair.second;
+            return First ^ (Second << 1); // XOR balances 0 and 1
+        }
+    };
+    // need to hash a pair
+    std::unordered_map<std::pair<TNodeID, TNodeID>,std::unordered_set<std::shared_ptr<SRoute>>, NodeIDPairHash> DRoutesByNodeID;
+
     
     SImplementation(std::shared_ptr<CBusSystem> bussystem){
         DBusSystem = bussystem;
+        // load in stop info
         for(size_t Index = 0; Index < DBusSystem->StopCount(); Index++){
             auto Stop = DBusSystem->StopByIndex(Index);
             DSortedStopsByIndex.push_back(Stop);
@@ -26,7 +37,8 @@ struct CBusSystemIndexer::SImplementation{
         std::sort(DSortedStopsByIndex.begin(), DSortedStopsByIndex.end(), [](std::shared_ptr<SStop> l, std::shared_ptr<SStop> r) -> bool{
             return l->ID() < r->ID();
         });
-
+        
+        // load in route info
         for(size_t Index = 0; Index < DBusSystem->RouteCount(); Index++) {
             auto Route = DBusSystem->RouteByIndex(Index);
             DSortedRoutesByIndex.push_back(Route);
@@ -35,6 +47,25 @@ struct CBusSystemIndexer::SImplementation{
         std::sort(DSortedRoutesByIndex.begin(), DSortedRoutesByIndex.end(), [](std::shared_ptr<SRoute> l, std::shared_ptr<SRoute> r) -> bool{
             return l->Name() < r->Name();
         });
+
+        // build our map for route by nodeID
+        for(auto Route: DSortedRoutesByIndex) {
+            // start at 1 so we can get prev and next stopID
+            for(size_t Index = 1; Index < Route->StopCount(); Index ++) {
+                auto Previous = Route->GetStopID(Index - 1);
+                auto Current = Route->GetStopID(Index);
+                auto FirstNodeID = DBusSystem->StopByID(Previous)->NodeID();
+                auto SecondNodeID = DBusSystem->StopByID(Current)->NodeID();
+                auto Key = std::make_pair(FirstNodeID, SecondNodeID);
+                // if key dne, put unordered set, else append to set
+                auto Search = DRoutesByNodeID.find(Key);
+                if (Search == DRoutesByNodeID.end()) {
+                    DRoutesByNodeID[Key] = {Route};
+                } else {
+                    Search->second.insert(Route);
+                }
+            }
+        }
     }
 
     ~SImplementation(){
@@ -54,7 +85,7 @@ struct CBusSystemIndexer::SImplementation{
     }
 
     std::shared_ptr<SRoute> SortedRouteByIndex(std::size_t index) const noexcept{
-        return DSortedRoutesByIndex[index];
+        return DSortedRoutesByIndex[index]; // stil need to do a check
     }
 
     std::shared_ptr<SStop> StopByNodeID(TNodeID id) const noexcept{
@@ -67,6 +98,16 @@ struct CBusSystemIndexer::SImplementation{
     }
 
     bool RoutesByNodeIDs(TNodeID src, TNodeID dest, std::unordered_set<std::shared_ptr<SRoute> > &routes) const noexcept{
+        // take pair of nodeID as a key and map to set of routes
+        auto Search =  DRoutesByNodeID.find(std::make_pair(src, dest));
+        if(Search != DRoutesByNodeID.end()) {
+            routes = Search->second;
+            // problem: if route A's pair is <102,101> but src/dest defined as <101,102>, route A never added
+            for(auto route: routes) {
+                std::cout << "route " << route->Name() << " has a stop between " << src << " and " << dest << std::endl;
+            }
+            return true;
+        }
         return false;
     }
 
