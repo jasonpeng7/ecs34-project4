e44ee6c8dcdf1b8093825164799d9cfea9d2f830
jasonpeng7 <144397187+jasonpeng7@users.noreply.github.com>
2026-03-08 00:04:12 +0000

    added custom test

--- testsrc/DijkstraPathRouterTest.cpp
+++ testsrc/DijkstraPathRouterTest.cpp
@@ -52,3 +52,49 @@ TEST(DijkstraPathRouter, ShortestPath){
     std::vector<CPathRouter::TVertexID> invalid_Path3;
     EXPECT_EQ(PathRouter.FindShortestPath(vertexB, vertexA, invalid_Path3), CPathRouter::NoPathExists);
 }
+
+TEST(DijkstraPathRouter, MyCustomGraphTest){
+    CDijkstraPathRouter PathRouter;
+
+    auto vertexA = PathRouter.AddVertex(std::string("A"));
+    auto vertexB = PathRouter.AddVertex(std::string("B"));
+    auto vertexC = PathRouter.AddVertex(std::string("C"));
+    auto vertexD = PathRouter.AddVertex(std::string("D"));
+    auto vertexE = PathRouter.AddVertex(std::string("E"));
+    auto vertexF = PathRouter.AddVertex(std::string("F"));
+    auto vertexG = PathRouter.AddVertex(std::string("G"));
+
+    EXPECT_EQ(PathRouter.VertexCount(), 7);
+    EXPECT_TRUE(PathRouter.AddEdge(vertexA, vertexB, 2.0));
+    EXPECT_TRUE(PathRouter.AddEdge(vertexA, vertexC, 9.0));
+
+    EXPECT_TRUE(PathRouter.AddEdge(vertexC, vertexB, 2.0));
+    EXPECT_TRUE(PathRouter.AddEdge(vertexC, vertexF, 6.0));
+    EXPECT_TRUE(PathRouter.AddEdge(vertexC, vertexD, 3.0));
+    EXPECT_TRUE(PathRouter.AddEdge(vertexC, vertexE, 3.0));
+
+    EXPECT_TRUE(PathRouter.AddEdge(vertexE, vertexG, 1.0));
+    EXPECT_TRUE(PathRouter.AddEdge(vertexE, vertexA, 10.0));
+
+    EXPECT_TRUE(PathRouter.AddEdge(vertexF, vertexB, 1.0));
+    EXPECT_TRUE(PathRouter.AddEdge(vertexF, vertexD, 4.0));
+
+    EXPECT_TRUE(PathRouter.AddEdge(vertexC, vertexF, 6.0));
+
+    EXPECT_TRUE(PathRouter.AddEdge(vertexG, vertexC, 3.0));
+
+    std::vector<CPathRouter::TVertexID> RegularPath1;
+    EXPECT_EQ(PathRouter.FindShortestPath(vertexA, vertexG, RegularPath1), 13.0);
+    std::vector<CPathRouter::TVertexID> ExpectedPath1{vertexA, vertexC, vertexE, vertexG};
+    EXPECT_EQ(RegularPath1, ExpectedPath1);
+
+    std::vector<CPathRouter::TVertexID> RegularPath2;
+    EXPECT_EQ(PathRouter.FindShortestPath(vertexG, vertexA, RegularPath2), 16.0);
+    std::vector<CPathRouter::TVertexID> ExpectedPath2{vertexG, vertexC, vertexE, vertexA};
+    EXPECT_EQ(RegularPath2, ExpectedPath2);
+}
+
+TEST(DijkstraPathRouter, BiredictionalEdges){
+    CDijkstraPathRouter PathRouter;
+
+}
