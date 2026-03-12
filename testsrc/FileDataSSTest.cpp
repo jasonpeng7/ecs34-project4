fd93552e0860041eb18d1fabca287a172768f62d
Andrew Kuang <40343154+andrewscoding2018@users.noreply.github.com>
2026-03-11 18:29:21 -0700

    FileData factory tests

--- testsrc/FileDataSSTest.cpp
+++ testsrc/FileDataSSTest.cpp
@@ -3,6 +3,7 @@
 #include "FileDataSink.h"
 #include "FileDataSource.h"
 #include <cstdio>
+#include <filesystem>
 
 // Assume being run from Makefile so testtmp is subdirectory
 
@@ -112,4 +113,66 @@ TEST(FileDataSourceSink, MissingSourceFailureStateTest){
     EXPECT_FALSE(Source->Peek(TempCh));
     EXPECT_FALSE(Source->Read(Buffer, 2));
     EXPECT_EQ(Buffer, std::vector<char>({'x'}));
+}
+
+TEST(FileDataSourceSink, EmptyBasePathDefaultsToCurrentDirectory){
+    CFileDataFactory DataFactory("");
+    std::string Filename = "filedatafactory_empty_base.txt";
+    std::remove(Filename.c_str());
+
+    {
+        auto Sink = DataFactory.CreateSink(Filename);
+        ASSERT_NE(Sink, nullptr);
+        EXPECT_TRUE(Sink->Put('Z'));
+    }
+
+    auto Source = DataFactory.CreateSource(Filename);
+    char TempCh = '\0';
+    EXPECT_TRUE(Source->Get(TempCh));
+    EXPECT_EQ(TempCh, 'Z');
+    EXPECT_TRUE(Source->End());
+
+    std::remove(Filename.c_str());
+}
+
+TEST(FileDataSourceSink, BasePathWithoutTrailingSlashIsNormalized){
+    std::string LocalBaseDirectory = "./testtmp/factory_normalized";
+    std::filesystem::remove_all(LocalBaseDirectory);
+
+    CFileDataFactory DataFactory(LocalBaseDirectory);
+    std::string Filename = "normalized.txt";
+
+    {
+        auto Sink = DataFactory.CreateSink(Filename);
+        ASSERT_NE(Sink, nullptr);
+        EXPECT_TRUE(Sink->Write(std::vector<char>({'O', 'K'})));
+    }
+
+    EXPECT_TRUE(std::filesystem::exists(LocalBaseDirectory + "/" + Filename));
+
+    auto Source = DataFactory.CreateSource(Filename);
+    std::vector<char> Buffer;
+    EXPECT_TRUE(Source->Read(Buffer, 2));
+    EXPECT_EQ(Buffer, std::vector<char>({'O', 'K'}));
+    EXPECT_TRUE(Source->End());
+
+    std::filesystem::remove_all(LocalBaseDirectory);
+}
+
+TEST(FileDataSourceSink, CreateSinkFailureReturnsNullptr){
+    std::string BlockingPath = BaseDirectory + "factory_blocker";
+    std::filesystem::remove_all(BlockingPath);
+    std::remove(BlockingPath.c_str());
+
+    {
+        FILE *BlockerFile = std::fopen(BlockingPath.c_str(), "w");
+        ASSERT_NE(BlockerFile, nullptr);
+        std::fputs("block", BlockerFile);
+        std::fclose(BlockerFile);
+    }
+
+    CFileDataFactory DataFactory(BlockingPath);
+    EXPECT_EQ(DataFactory.CreateSink("blocked.txt"), nullptr);
+
+    std::remove(BlockingPath.c_str());
 }
